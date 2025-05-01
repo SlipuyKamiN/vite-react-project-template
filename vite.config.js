1f01233cd0d88a2d6b1723689bae36404c295d91
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-05-01 18:35:37 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "vite-react-project-template",
+  base: "/vite-react-project-template/",
   plugins: [react()],
 });
